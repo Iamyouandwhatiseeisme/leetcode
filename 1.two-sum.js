82dde1c84c38e9ba0ccb048bebb8007e8f7c7853
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-16 20:46:36 +0400

    1# solved

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -10,8 +10,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  var x;
+  var y;
+  for (var i = 0; i <= nums.length; i++) {
+    x = nums[i];
+    for (var j = i + 1; j <= nums.length; j++) {
+      if (nums[j] + x === target) {
+        console.log(i, j);
+        return [i, j];
+      }
+    }
+  }
 };
+twoSum([2, 11, 7, 15], 9);
 // @lc code=end
-
